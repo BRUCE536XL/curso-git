51e391b104d36ce2362499625c05c4d324c460cc
bruce535xl <bobbyclaytton@hotmail.com>
2024-02-08 19:57:39 -0300

    salvando o arquivo js

--- projetos1/script.js
+++ projetos1/script.js
@@ -1,54 +1,42 @@
+var nao_e = 0;
 
-var botao = document.createElement("button");
-var nao_e =0
 function fuja() {
+    document.getElementById("nao_e").textContent = "TENTOU " + nao_e; // Atualiza o texto do parágrafo
     var botaonao = document.getElementById("nao");
     var largurajanela = window.innerWidth;
     var alturaJanela = window.innerHeight;
-    document.getElementById("nao_e").textContent = "TENTOU " + nao_e; // Atualiza o texto do parágrafo
-    nao_e ++
+  
+    nao_e++;
 
     var margem = -5; // Margem para manter o botão longe da borda
+    var larguraLimite = 700; // Largura limite da área
+    var alturaLimite = 500; // Altura limite da área
 
-    var maxX = largurajanela - botaonao.offsetWidth - margem;
-    var maxY = alturaJanela - botaonao.offsetHeight - margem;
+    var maxX = Math.min(largurajanela - larguraLimite, largurajanela - botaonao.offsetWidth - margem);
+    var maxY = Math.min(alturaJanela - alturaLimite, alturaJanela - botaonao.offsetHeight - margem);
 
     var aleatorioX = Math.max(Math.floor(Math.random() * maxX), margem);
     var aleatorioY = Math.max(Math.floor(Math.random() * maxY), margem);
-
-
 
     botaonao.style.left = aleatorioX + "px";
     botaonao.style.top = aleatorioY + "px";
 }
 
-
 function clik() {
     var foto = document.getElementById("foto");
-    foto.width = 200;
-    foto.Height = 200
-    foto.src = "https://i.pinimg.com/originals/6f/4e/31/6f4e31c19d69356a7ee7edb19e216ff7.gif"; 
+    foto.width = 600; // Nova largura desejada
+    foto.height = 300; // Nova altura calculada para manter a proporção (200 * 0.75)
     
-    //////////////////////////////////////////////////////////////////////////////////////////
-    // Criar um botão
-var botao = document.createElement("button");
+    foto.src = "https://i.pinimg.com/originals/6f/4e/31/6f4e31c19d69356a7ee7edb19e216ff7.gif";
 
-// Definir o texto do botão
-botao.textContent = "Resetar Página";
-
-// Adicionar um identificador (ID) ao botão (opcional)
-botao.id = "resetButton";
-
-// Adicionar uma classe ao botão (opcional)
-botao.classList.add("btn", "btn-primary");
-
-// Adicionar um evento de clique ao botão
-botao.addEventListener("click", function() {
+    // Adicionar um botão para resetar a página
+    var botao = document.createElement("button");
+    botao.textContent = "Resetar Página";
+    botao.id = "resetButton";
+    botao.classList.add("btn", "btn-primary");
+    botao.addEventListener("click", function() {
     location.reload(); // Recarregar a página
-});
-
-// Adicionar o botão ao corpo do documento
-document.body.appendChild(botao);
-
-
+    });
+    document.body.appendChild(botao);
+    
 }
